453f627a09140e628e35f65316df56e701f71b68
Kelly <kgilliamcode@gmail.com>
2017-12-20 17:19:47 -0800

    route to chatRoom on login with setState

--- components/landing/login.jsx
+++ components/landing/login.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Redirect } from 'react-router'
 import { Link } from 'react-router-dom';
 import styles from './landing.css';
 import TextField from 'material-ui/TextField';
 import FlatButton from 'material-ui/FlatButton';
-import {orange500, deepOrange500, green500} from 'material-ui/styles/colors';
+import {orange500, deepOrange500 } from 'material-ui/styles/colors';
 import axios from 'axios';
 
+// material ui styles
 const fieldStyles = {
 	colorOrange: {
 		color: orange500,
@@ -19,11 +22,13 @@ const fieldStyles = {
 class Login extends Component {
 	constructor(props, context) {
 		super(props, context);
-		this.state = {};
+		this.state = {
+			redirectToChatRoom: false
+		};
 		this.submitLoginCredentials = this.submitLoginCredentials.bind(this);
 	}
 
-	submitLoginCredentials (nameVal, passwordVal) {
+	submitLoginCredentials (nameVal, passwordVal, that) {
     axios.post('/checkCredentials', {
       name: nameVal,
       password: passwordVal
@@ -32,12 +37,12 @@ class Login extends Component {
       console.log(response);
       document.getElementById("nameField").value = '', 
       document.getElementById("passwordField").value = ''
-      // response data is an empty string this means the user isnt found in the database. 
-      // alert the user of incorrect credentials
+      // if response data is an empty string this means the user isnt found in the database. 
+      // if is empty, alert the user of incorrect credentials
       if (response.data === '') alert ('incorrect password or username');
-			// else that.setState({showLogin: 'none'});
 			else {
-				axios.post('/checkCredentials', {})
+				// re-render and route to chatroom with react router
+				that.setState({redirectToChatRoom: true});
 			}
     })
     .catch(function (error) {
@@ -46,6 +51,10 @@ class Login extends Component {
   }
 	
 	render() {
+		// if credentials come back true, redirect to chatroom:
+		if (this.state.redirectToChatRoom === true) return (<Redirect to='/chatroom'/>);
+		// otherwise render login page
+		else {
 		return (
 			<div>
 				<div className="landingBox">
@@ -65,13 +74,14 @@ class Login extends Component {
 							this.submitLoginCredentials(
 								document.getElementById("nameField").value, 
 								document.getElementById("passwordField").value, 
+								this
 							)
 							}}/>
 					</div>
-					<li><Link to='/chatroom'>ChatRoom</Link></li>
 				</div>
 			</div>
 				)
+			}
 		}
 }
 
